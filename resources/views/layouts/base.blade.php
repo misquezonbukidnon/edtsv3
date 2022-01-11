efd0472f9fac39a850538bb638f8fc044a722a3d
Jayson R. Alanano <json.alanano@gmail.com>
2022-01-11 13:46:40 +0800

    BE 011122
    Alpine JS

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -1,17 +1,18 @@
 <!DOCTYPE html>
 <html class="h-full bg-gray-100" lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         @hasSection('title')
 
-            <title>@yield('title') - {{ config('app.name') }}</title>
+        <title>@yield('title') - {{ config('app.name') }}</title>
         @else
-            <title>{{ config('app.name') }}</title>
+        <title>{{ config('app.name') }}</title>
         @endif
 
         <!-- Favicon -->
-		<link rel="shortcut icon" href="{{ url(asset('favicon.ico')) }}">
+        <link rel="shortcut icon" href="{{ url(asset('favicon.ico')) }}">
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
@@ -21,6 +22,7 @@
         @livewireStyles
 
         <!-- Scripts -->
+        <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
         <script src="{{ url(mix('js/app.js')) }}" defer></script>
 
         <!-- CSRF Token -->
@@ -32,4 +34,5 @@
 
         @livewireScripts
     </body>
+
 </html>
