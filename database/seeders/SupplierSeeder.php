d3c9575ce1e35605f7cf580e8c7f6f2421e7765d
MrRegei <39606446+MrRegei@users.noreply.github.com>
2022-01-27 11:54:28 +0800

    FE- 12722
    Supplier Seeder

--- database/seeders/SupplierSeeder.php
+++ database/seeders/SupplierSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class SupplierSeeder extends Seeder
 {
@@ -13,6 +14,318 @@ class SupplierSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('suppliers')->insert([
+            [
+                'name' => 'Kiokong Construction & Development Corporation',
+                'address' => 'Quezon, Bukidnon'
+            ],
+            [
+                'name' => 'FP Alonto Construction Services',
+                'address' => 'Marawi City'
+            ],
+            [
+                'name' => 'Mafil Builders',
+                'address' => 'Valencia City, Bukidnon'
+            ],
+            [
+                'name' => 'Oresons Construction Services',
+                'address' => 'Quezon, Bukidnon'
+            ],
+            [
+                'name' => 'DL Builders & Real Properties Development, Inc.',
+                'address' => 'Valencia City, Bukidnon'
+            ],
+            [
+                'name' => 'ELC Construction & Trading',
+                'address' => 'Cagayan de Oro City'
+            ],
+            [
+                'name' => 'RRJ Hardware & Construction Supplies',
+                'address' => 'Quezon, Bukidnon'
+            ],
+            [
+                'name' => 'Orion Shell Station',
+                'address' => 'Quezon, Bukidnon'
+            ],
+            [
+                'name' => 'Philmarc Food Services, Inc.',
+                'address' => 'Quezon, Bukidnon'
+            ],
+            [
+                'name' => 'Ria Ruby Pension House & Food Services, Inc.',
+                'address' => 'Quezon, Bukidnon'
+            ],
+            [
+                'name' => 'M-Rich Store & Food Services',
+                'address' => 'Quezon, Bukidnon'
+            ],
+            [
+                'name' => 'GB Ilonggo Restaurant',
+                'address' => 'Quezon, Bukidnon'
+            ],
+            [
+                'name' => 'Kimlong Office & School Supplies Trading',
+                'address' => 'Butong, Quezon, Buidnon'
+            ],
+            [
+                'name' => 'Centro Supersales',
+                'address' => 'Valencia City, Bukidnon'
+            ],
+            [
+                'name' => 'SB Store',
+                'address' => 'Quezon, Bukidnon'
+            ],
+            [
+                'name' => 'MA Catalan Store',
+                'address' => 'Salawagan, Quezon, Bukidnon'
+            ],
+            [
+                'name' => 'Tabafia Rice & Corn Retail',
+                'address' => 'Quezon, Bukidnon'
+            ],
+            [
+                'name' => 'WG & G Enterprises',
+                'address' => 'Quezon, Bukidnon'
+            ],
+            [
+                'name' => 'Israel Hardware & Electrical Supplies General Merchandise',
+                'address' => 'Maramag, Bukidnon'
+            ],
+            [
+                'name' => 'Mindanao ER Electrical Supply',
+                'address' => 'Maramag, Bukidnon'
+            ],
+            [
+                'name' => 'Diamond Star Industrial Trading',
+                'address' => 'Maramag, Bukidnon'
+            ],
+            [
+                'name' => 'Camp 1 Marketing',
+                'address' => 'Maramag, Bukidnon'
+            ],
+            [
+                'name' => 'Benz General Merchandise',
+                'address' => 'Cagayan de Oro City'
+            ],
+            [
+                'name' => 'Mangubat Enterprises',
+                'address' => 'Quezon, Bukidnon'
+            ],
+            [
+                'name' => 'Golden Arrow Electrocare Corporation',
+                'address' => 'Cagayan de Oro City'
+            ],
+            [
+                'name' => 'JC Auto Repair Shop & Auto Parts Supply',
+                'address' => 'Cagayan de Oro City'
+            ],
+            [
+                'name' => 'Valencia Goodwill Inc.',
+                'address' => 'Valencia City, Bukidnon'
+            ],
+            [
+                'name' => 'Rainwell Machine Shop',
+                'address' => 'Valencia City, Bukidnon'
+            ],
+            [
+                'name' => 'E-Movers Heavy Equipment Parts & Supply',
+                'address' => 'Maramag, Bukidnon'
+            ],
+            [
+                'name' => 'Johnbest Calibration',
+                'address' => 'Valencia City, Bukidnon'
+            ],
+            [
+                'name' => 'Emcor, Inc.',
+                'address' => 'Quezon, Bukidnon'
+            ],
+            [
+                'name' => 'Darbs Corporation',
+                'address' => 'Cagayan de Oro City'
+            ],
+            [
+                'name' => 'GVG Furniture Trading',
+                'address' => 'Davao City'
+            ],
+            [
+                'name' => 'Summit Furnishings Inc.',
+                'address' => 'Davao City'
+            ],
+            [
+                'name' => 'Galaxy Construction Supplies & Services',
+                'address' => 'Cagayan de Oro City'
+            ],
+            [
+                'name' => 'Freezing Point Airconditioning System',
+                'address' => 'Cagayan de Oro City'
+            ],
+            [
+                'name' => 'JDM Cold Refrigeration & Aircon Parts & Services',
+                'address' => 'Maramag, Bukidnon'
+            ],
+            [
+                'name' => 'Ariaz General Merchandise',
+                'address' => 'Cotabato City'
+            ],
+            [
+                'name' => 'Palmer Asia Inc.',
+                'address' => 'Makati City'
+            ],
+            [
+                'name' => 'Toyota Valencia, Inc.',
+                'address' => 'Valencia City, Bukidnon'
+            ],
+            [
+                'name' => 'Superior Motors Corporation',
+                'address' => 'Valencia City, Bukidnon'
+            ],
+            [
+                'name' => 'CCTR Enterprises',
+                'address' => 'Davao City'
+            ],
+            [
+                'name' => 'Execom Computer Sales & Services',
+                'address' => 'Maramag, Bukidnon'
+            ],
+            [
+                'name' => 'Bisswiz Computer Center',
+                'address' => 'Valecia City, Bukidnon'
+            ],
+            [
+                'name' => 'EAH Medicine & Medical Supplies Marketing',
+                'address' => 'Davao City'
+            ],
+            [
+                'name' => 'Aton Pharma',
+                'address' => 'Cagayan de Oro City'
+            ],
+            [
+                'name' => 'Prodemic Pharma',
+                'address' => 'Cagayan de Oro City'
+            ],
+            [
+                'name' => 'Savelink Pharma & Trading Corp.',
+                'address' => 'Cagayan de Oro City'
+            ],
+            [
+                'name' => 'RM Pharmamils Supplies',
+                'address' => 'Davao del Norte'
+            ],
+            [
+                'name' => 'RBJ Commodities Trading Exponent',
+                'address' => 'Cagayan de Oro City'
+            ],
+            [
+                'name' => 'SNEV Merchandise',
+                'address' => 'Salawagan, Quezon, Bukidnon'
+            ],
+            [
+                'name' => 'LR Enterprises',
+                'address' => 'Malaybalay City'
+            ],
+            [
+                'name' => 'Shanel & Louis Enterprises',
+                'address' => 'Davao City'
+            ],
+            [
+                'name' => 'Ormin Pharmacueticals ',
+                'address' => 'Calapan City, Oriental Mindoro'
+            ],
+            [
+                'name' => 'RD Trading',
+                'address' => 'Quezon, Bukidnon'
+            ],
+            [
+                'name' => 'HLM Leverage Pharmacuetical Corp.',
+                'address' => 'Iligan City'
+            ],
+            [
+                'name' => 'NTT Agri Supply',
+                'address' => 'Malaybalay City'
+            ],
+            [
+                'name' => 'Green and Grow Teknology',
+                'address' => 'Los Baños Laguna'
+            ],
+            [
+                'name' => 'Micro Merchandise',
+                'address' => 'Maramag, Bukidnon'
+            ],
+            [
+                'name' => 'BUKTRAMCO',
+                'address' => 'Quezon, Bukidnon'
+            ],
+            [
+                'name' => 'Bukidnon Dispatch',
+                'address' => 'Valencia City, Bukidnon'
+            ],
+            [
+                'name' => 'Philippine Duplicators, Inc.',
+                'address' => 'Cagayan de Oro City'
+            ],
+            [
+                'name' => 'Fortunato Cabarde, Sr.',
+                'address' => 'Quezon, Bukidnon'
+            ],
+            [
+                'name' => 'Ramon Ramirez, Jr. Group',
+                'address' => 'Quezon, Bukidnon'
+            ],
+            [
+                'name' => 'Jomar Nogalada',
+                'address' => 'Quezon, Bukidnon'
+            ],
+            [
+                'name' => 'Armscor Shooting Center, Inc.',
+                'address' => 'Davao City'
+            ],
+            [
+                'name' => 'Microtrade GMC Corp.',
+                'address' => 'Malaybalay City'
+            ],
+            [
+                'name' => 'Foorprints Award Centrum',
+                'address' => 'Cagayan de Oro City'
+            ],
+            [
+                'name' => 'Streck Trading Inc.',
+                'address' => 'Valencia City, Bukidnon'
+            ],
+            [
+                'name' => 'Inkthusiast Printing Shop',
+                'address' => 'Quezon, Bukidnon'
+            ],
+            [
+                'name' => "Marcy's Corporation",
+                'address' => 'Valencia City, Bukidnon'
+            ],
+            [
+                'name' => 'TKG Mobile Lights & Sounds',
+                'address' => 'Maramag, Bukidnon'
+            ],
+            [
+                'name' => 'Tower Fireworks',
+                'address' => 'North Cotabato'
+            ],
+            [
+                'name' => 'For Ella & Eman Advertising',
+                'address' => 'Maramag, Bukidnon'
+            ],
+            [
+                'name' => 'PTO Bukidnon',
+                'address' => 'Malaybalay City'
+            ],
+            [
+                'name' => 'Newborn Screening Center-Mindanao',
+                'address' => 'Davao City'
+            ],
+            [
+                'name' => 'D.A. -10LN2',
+                'address' => 'Malaybalay City'
+            ],
+
+            
+
+        ]);
     }
 }
