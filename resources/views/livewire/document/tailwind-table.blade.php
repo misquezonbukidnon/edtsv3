dc43ef4c911b2a5b8eb6c5dd4ed877223328005b
MrRegei <39606446+MrRegei@users.noreply.github.com>
2022-04-13 10:08:56 +0800

    FE 041322
    Datatable Action Button

--- resources/views/livewire/document/tailwind-table.blade.php
+++ resources/views/livewire/document/tailwind-table.blade.php
@@ -166,8 +166,25 @@
                                     <td class="hidden px-3 py-4 text-sm text-gray-500 lg:table-cell">
                                         {{ $document->status->name }}
                                     </td>
-                                    <td class="px-3 py-4 text-sm text-gray-500 sm:table-cell">
-                                        <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
+                                    <td class="px-3 py-4  text-sm text-gray-500 sm:table-cell flex">
+                                        <div class="flex">
+                                            <a href="#" class="text-indigo-600 hover:text-indigo-900 mr-1"><svg
+                                                    xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none"
+                                                    viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                                        d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+                                                </svg>
+                                            </a>
+                                            <a href="#" class="text-indigo-600 hover:text-indigo-900">
+                                                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none"
+                                                    viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                                        d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                                        d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+                                                </svg>
+                                            </a>
+                                        </div>
                                     </td>
                                 </tr>
                                 @endforeach
